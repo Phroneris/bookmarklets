32deee3cb7f65886f68ed330e6a94133e65a6920
Phroneris <phroneris@gmail.com>
2019-08-08 18:43:01 +0900

    現ページタイトル＆URL取得 v1.0.1

--- full_promptPageTitleAndURL.js
+++ full_promptPageTitleAndURL.js
@@ -1,32 +1,43 @@
 javascript:(
 	/*
-		開いているページのタイトルとURLをプロンプトウィンドウに表示して取得
-		ニコ動・ニコ静・pixiv（イラスト、小説、ユーザーページ）の場合は専用の短縮URL化
+		「現ページタイトル＆URL取得」
+		
+		現在開いているページのタイトルとURLをプロンプトウィンドウに表示して取得。
+		ニコ動・ニコ静・pixiv（イラスト、小説、ユーザーページ）の場合は専用の短縮URL化。
 	*/
-	function() {
-		var url = location.href;
-		var pre = '^https?://';
-		var esc = function(s) { return s.replace(/([\.\/])/g, '\\$1') };
-		var nptn = pre + '(?:www.nicovideo.jp/watch/|seiga.nicovideo.jp/seiga/)([si]m\\d+)';
-		var pptn = pre + 'www.pixiv.net/';
-		var nreg = new RegExp(esc(nptn)+'.*$');
-		var preg = new RegExp(esc(pptn));
-		if (url.match(nreg)) {
-			url = url.replace(nreg, 'http://nico.ms/$1')
-		} else if (url.match(preg)) {
-			var f = [
+	() => {
+		let url = location.href;
+		const esc = s => s.replace(/([\.\/])/g, String.raw`\$1`);
+		const nPtn = String.raw`^https?://(?:www.nicovideo.jp/watch/|seiga.nicovideo.jp/seiga/)([si]m\d+)`;
+		const pPtn = '^https?://www.pixiv.net/';
+		const nReg = new RegExp(`${esc(nPtn)}.*$`);
+		let pReg = new RegExp(esc(pPtn));
+		if (url.match(nReg)) {
+			url = url.replace(nReg, 'https://nico.ms/$1');
+		} else if (url.match(pReg)) {
+			const f = [
 				['member.php', 'id', 'u'],
 				['member_illust.php', 'illust_id', 'i'],
 				['novel/show.php', 'id', 'n']
 			];
-			for (var i=0; i<f.length; i++) {
-				preg = new RegExp(esc(pptn + f[i][0] + '\\?(?:[^&]+&)?' + f[i][1] + '=(\\d+)') + '.*$');
-				if (url.match(preg)) {
-					url = url.replace(preg, 'https://pixiv.net/' + f[i][2] + '/$1');
-					break
+			for (let i=0; i<f.length; i++) {
+				pReg = new RegExp(String.raw`${esc(pPtn)}${f[i][0]}\?(?:[^&]+&)?${f[i][1]}=(\d+).*$`);
+				// console.log(pReg);
+				if (url.match(pReg)) {
+					url = url.replace(pReg, `https://pixiv.net/${f[i][2]}/$1`);
+					break;
 				}
 			}
-		}
-		prompt('Title%20+%20URL', document.title + ' ' + url)
+		}; // minifier.org用セミコロン
+		prompt('Title + URL', `${document.title} ${url}`);
 	}
+	/*
+		2019/08/08
+		v1.0.1
+		・なんか強そうな書き方に挑戦。
+		
+		2018/07/05
+		v1.0.0
+		・公開。
+	*/
 )();
